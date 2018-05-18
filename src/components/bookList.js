d4d5837a1b7914b68185482c549c40577b21da08
oahmaro <osamaahmaro@gmail.com>
2018-05-18 13:46:35 +0300

    feat: Add a bookList component

---
+++ src/components/bookList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class bookList extends Component {
+    render() {
+        return(
+            <p>Shelves Go Here</p>
+        )
+    }
+}
+
+export default booklist;
